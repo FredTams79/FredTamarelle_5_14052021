e82a8d336e74f43b21829d51cb09e9b26dc2f900
FredTams79 <fred.tamarelle@gmail.com>
2021-05-12 00:29:05 +0200

    mise en place liens dynamique index.html

--- JS/products.js
+++ JS/products.js
@@ -33,22 +33,4 @@ const quantity = document.getElementById("quantityProduct").value;
 /// Envoyer notre choix au panier ///
 getElementById("basketProduct");*/
 
-/*//// Récupérer les données des produits par leur _id /////
-
-fetch("http://localhost:3000/api/furniture") //récupére la ressource à l'adress url
-  .then((response) => response.json())
-  .then(function (data) {
-    const furnitureName = document.getElementById("furnitureName");
-    return data.map(function (meuble) {
-      let div = document.createElement("div");
-      let a = document.createElement("a");
-      a.setAttribute("href", "products.html?id=" + meuble._id);
-      //a.innerHTML = "afficher le produit";
-      div.innerHTML = meuble.name;
-      div.setAttribute("class", "card-title");
-      div.appendChild(a);
-      furnitureName.appendChild(div);
-    });
-  })
-  .catch((error) => console.log(error)); // Une erreur est surv
-*/
+//// Récupérer les données des produits par leur _id /////
--- JS/scripts.js
+++ JS/scripts.js
@@ -11,17 +11,49 @@ $(".list-hours li").eq(new Date().getDay()).addClass("today");
 fetch("http://localhost:3000/api/furniture") //récupére la ressource à l'adress url
   .then((response) => response.json())
   .then(function (data) {
-    const furnitureName = document.getElementById("furnitureName");
+    const furnitureProducts = document.getElementById("furnitureProducts");
     return data.map(function (meuble) {
-      //let div = document.createElement("div");
-      let a = document.createElement("a");
-      a.setAttribute("href", "products.html?id=" + meuble._id);
-      a.innerHTML = meuble.name;
-      //div.innerHTML = meuble.name;
-      a.setAttribute("class", "card-title");
-      //div.appendChild(a);
-      furnitureName.appendChild(a);
-      console.log(meuble.name);
+      const furnitureProduct = document.createElement("div");
+      const imageProduct = document.createElement("img");
+      const furnitureDetail = document.createElement("div");
+      const furnitureName = document.createElement("h4");
+      const furnitureDescription = document.createElement("p");
+      const furniturePrice = document.createElement("p");
+      const productLink = document.createElement("a");
+
+      furnitureProduct.setAttribute(
+        "class",
+        "card col-lg-3 col-sm-5 col-10 px-0 mx-1 mb-3"
+      );
+      imageProduct.setAttribute("class", "card-img-top");
+      imageProduct.setAttribute("src", meuble.imageUrl);
+      imageProduct.setAttribute("alt", "photo de " + meuble.name);
+      furnitureDetail.setAttribute(
+        "class",
+        "card-body d-flex flex-column justify-content-between"
+      );
+      furnitureName.setAttribute("class", "card-title");
+      furnitureDescription.setAttribute("class", "card-text");
+      furniturePrice.setAttribute("class", "card-price");
+      productLink.setAttribute("href", "products.html?id=" + meuble._id);
+      productLink.setAttribute("class", "btn btn-primary");
+
+      furnitureName.innerHTML = meuble.name;
+      furnitureDescription.innerHTML = meuble.description;
+      furniturePrice.innerHTML = "Prix : " + meuble.price / 100 + " €";
+      productLink.innerHTML =
+        '<i class="fas fa-cart-arrow-down fa-2x"></i></br>Voir la page :</br>' +
+        meuble.name;
+
+      furnitureProducts.appendChild(furnitureProduct);
+      furnitureProduct.appendChild(imageProduct);
+      furnitureProduct.appendChild(furnitureDetail);
+      furnitureDetail.appendChild(furnitureName);
+      furnitureDetail.appendChild(furnitureDescription);
+      furnitureDetail.appendChild(furniturePrice);
+      furnitureDetail.appendChild(productLink);
+
+      console.log(meuble);
     });
   })
   .catch((error) => console.log(error)); // Une erreur est survenue
